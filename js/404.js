23be73f5535eb492888f43df4c1107a0dadfd34f
MyBooty165 <126267638+MyBooty165@users.noreply.github.com>
2023-09-29 06:45:56 -0500

    Update 404.js

--- js/404.js
+++ js/404.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   if ($(".error-container").length > 0) {
     $.ajax({
-      url: "404.html", // Update the URL to the path of your 404.html page
+      url: "/error/404.html", // Update the URL to the path of your 404.html page
       dataType: "html",
       success: function(data) {
         $(".error-container").html(data);
