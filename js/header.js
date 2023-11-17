541fd78d7eb961ac4407f9e356e46c2ea748dafe
X-88 <126267638+MyBooty165@users.noreply.github.com>
2023-11-17 02:29:08 +0000

    Create header.js

---
+++ js/header.js
@@ -0,0 +1,23 @@
+// so uhh the header is tedious to update, srry scott
+document.write(`
+  <header>
+    <a class="title" href="/">
+      <img src="/img/favicon/cybria-games-64x64.png" width="128" height="128">
+    </a>
+    <nav>
+      <a href="https://www.buymeacoffee.com/cybriatech" target="_blank"><i class="fas fa-heart" style="color: #FF5733;"></i> Fund</a>
+      <a href="https://discord.gg/e2UXEwjcsg"><i class="fab fa-discord" style="color: #7289DA;"></i> Discord</a>
+      <a href="https://github.com/CybriaTech/CybriaGames"><i class="fab fa-github"></i> GitHub</a>
+      <a href="mirrors.html"><i class="fas fa-layer-group"></i> Mirrors</a>
+      <a href="report.html"><i class="fas fa-comments" style="color: #ffffff;"></i> Report A Game</a> 
+      <a href="mailto:timmytamle569@gmail.com"><i class="fas fa-envelope"></i> Contact</a>
+    </nav>
+  </header>
+  <br>
+  <form id="search-container">
+    <div id="logo-title">
+      <h1>CybriaGames</h1>
+    </div>
+    <input id="search-input" placeholder="Search For A Game...">
+  </form>
+`);
