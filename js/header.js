7b4f98b3d5eaffb7c08465e74cf322eff8653b70
X-88 <126267638+MyBooty165@users.noreply.github.com>
2023-12-04 16:13:34 +0000

    Update header.js

--- js/header.js
+++ js/header.js
@@ -2,12 +2,12 @@
 document.write(`
   <header>
     <a class="title" href="/">
-      <img src="/img/favicon/ii-64x64.png" width="128" height="128">
+      <img src="/img/favicon/cybria-games-64x64.png" width="128" height="128">
     </a>
     <nav>
-      <a href="https://www.buymeacoffee.com/imperium-inc" target="_blank"><i class="fas fa-heart" style="color: #FF5733;"></i> Fund</a>
+      <a href="https://www.buymeacoffee.com/cybriatech" target="_blank"><i class="fas fa-heart" style="color: #FF5733;"></i> Fund</a>
       <a href="https://discord.gg/e2UXEwjcsg"><i class="fab fa-discord" style="color: #7289DA;"></i> Discord</a>
-      <a href="https://github.com/CybriaTech/Imperium-Inc"><i class="fab fa-github"></i> GitHub</a>
+      <a href="https://github.com/CybriaTech/Imperium-Inc/CybriaGames"><i class="fab fa-github"></i> GitHub</a>
       <a href="support.html"><i class="fas fa-flag"></i> Support</a>
       <a href="about.html"><i class="fas fa-users"></i> About Us</a> 
     </nav>
