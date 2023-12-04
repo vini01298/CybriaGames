211b06d461489230a5c55641d6b312f990f67dd9
X-88 <126267638+MyBooty165@users.noreply.github.com>
2023-12-04 20:33:09 +0000

    Update header.js

--- js/header.js
+++ js/header.js
@@ -7,7 +7,7 @@ document.write(`
     <nav>
       <a href="https://www.buymeacoffee.com/cybriatech" target="_blank"><i class="fas fa-heart" style="color: #FF5733;"></i> Fund</a>
       <a href="https://discord.gg/e2UXEwjcsg"><i class="fab fa-discord" style="color: #7289DA;"></i> Discord</a>
-      <a href="https://github.com/CybriaTech/Imperium-Inc/CybriaGames"><i class="fab fa-github"></i> GitHub</a>
+      <a href="https://github.com/CybriaTech/CybriaTech/CybriaGames"><i class="fab fa-github"></i> GitHub</a>
       <a href="support.html"><i class="fas fa-flag"></i> Support</a>
       <a href="about.html"><i class="fas fa-users"></i> About Us</a> 
     </nav>
